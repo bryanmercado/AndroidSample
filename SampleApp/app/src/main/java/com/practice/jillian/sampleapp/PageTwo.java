04bbfd7d35f06acb92cd6644f05a1fde67f41546
jilliankristi <jilliandeligero@gmail.com>
2014-10-26 20:41:59 +0800

    Copy Files to Github

---
+++ SampleApp/app/src/main/java/com/practice/jillian/sampleapp/PageTwo.java
@@ -0,0 +1,36 @@
+package com.practice.jillian.sampleapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class PageTwo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_two);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.page_two, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ SampleApp/app/src/main/java/com/practice/jillian/sampleapp/Timer.java
@@ -0,0 +1,112 @@
+package com.practice.jillian.sampleapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
+public class Timer extends Activity implements View.OnClickListener {
+
+    Button addButton;
+    Button subButton;
+    Button mulButton;
+    Button divButton;
+    EditText input1;
+    EditText input2;
+    TextView answerView;
+
+    Button nextButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+
+        addButton = (Button)findViewById(R.id.addButton);
+        addButton.setOnClickListener(this);
+
+        subButton = (Button)findViewById(R.id.minusButton);
+        subButton.setOnClickListener(this);
+
+        mulButton = (Button)findViewById(R.id.multiplyButton);
+        mulButton.setOnClickListener(this);
+
+        divButton = (Button)findViewById(R.id.divideButton);
+        divButton.setOnClickListener(this);
+
+        nextButton = (Button) findViewById(R.id.nextPageButton);
+        /*nextButton.setOnClickListener(this);*/
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Timer.this, PageTwo.class);
+                startActivity(myIntent);
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.timer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        /*final Context context = this;*/
+
+        int answer = 0;
+
+        input1 = (EditText)findViewById(R.id.editText2);
+        input2 = (EditText)findViewById(R.id.editText3);
+
+        if(!input1.getText().equals("") || !input1.getText().equals("")) {
+            switch (v.getId()) {
+                case R.id.addButton:
+                    answer = Integer.parseInt(input1.getText().toString()) + Integer.parseInt(input2.getText().toString());
+                    break;
+                case R.id.minusButton:
+                    answer = Integer.parseInt(input1.getText().toString()) - Integer.parseInt(input2.getText().toString());
+                    break;
+                case R.id.multiplyButton:
+                    answer = Integer.parseInt(input1.getText().toString()) * Integer.parseInt(input2.getText().toString());
+                    break;
+                case R.id.divideButton:
+                    answer = Integer.parseInt(input1.getText().toString()) / Integer.parseInt(input2.getText().toString());
+                    break;
+            }
+        }
+
+        answerView = (TextView)findViewById(R.id.textView5);
+        answerView.setText(Integer.toString(answer));
+    }
+
+    public void nextPage(View v){
+        Intent intent = new Intent(this, PageTwo.class);
+        startActivity(intent);
+    }
+}
